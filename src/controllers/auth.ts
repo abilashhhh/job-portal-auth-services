419da6d8aeb237f6f5aba79b75503f31bb2b622c
Abilash N <abilashnarayanan2001@gmail.com>
2026-02-26 15:16:14 +0530

    Login Route completed

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -37,14 +37,12 @@ export const registerUser = TryCatch(async (req, res, next) => {
       throw new ErrorHandler(400, "Failed to generate buffer");
     }
 
-    const {data} = await axios.post(
+    const { data } = await axios.post(
       `${process.env.UPLOAD_SERVICE_URL}/api/utils/upload`,
       {
         buffer: fileBuffer.content,
       },
     );
-
-    console.log("Upload service response:", data);
 
     const [user] =
       await sql`INSERT INTO users (name, email, password, phone_number, role, bio, resume, resume_public_id) 
@@ -54,10 +52,62 @@ export const registerUser = TryCatch(async (req, res, next) => {
   }
 
   const token = jwt.sign(
-    { id: registeredUser?.user_id},
+    { id: registeredUser?.user_id },
     process.env.JWT_SECRET as string,
     { expiresIn: "7d" },
   );
 
   res.json({ message: "User registered successfully", registeredUser, token });
+});
+
+export const loginUser = TryCatch(async (req, res, next) => {
+  const { email, password } = req.body;
+  if (!email || !password)
+    throw new ErrorHandler(400, "Email and password are required");
+
+  const user = await sql`
+  SELECT 
+    u.user_id,
+    u.name,
+    u.email,
+    u.password,
+    u.phone_number,
+    u.role,
+    u.bio,
+    u.resume,
+    u.profile_pic,
+    u.subscription,
+    ARRAY_AGG(s.name) FILTER (WHERE s.name IS NOT NULL) AS skills
+  FROM users u
+  LEFT JOIN user_skills us ON u.user_id = us.user_id
+  LEFT JOIN skills s ON us.skill_id = s.skill_id
+  WHERE u.email = ${email}
+  GROUP BY u.user_id;
+`;
+
+  if (user.length === 0) throw new ErrorHandler(400, "Invalid credentials");
+
+  const userObject = user[0];
+  if (!userObject.password || typeof userObject.password !== "string") {
+    throw new ErrorHandler(400, "Invalid credentials");
+  }
+
+  const matchPassword = await bcrypt.compare(
+    String(password),
+    userObject.password,
+  );
+  if (!matchPassword) throw new ErrorHandler(400, "Invalid credentials");
+
+
+  
+  userObject.skills = userObject.skills || [];
+  delete userObject.password;
+
+  const token = jwt.sign(
+    { id: userObject?.user_id },
+    process.env.JWT_SECRET as string,
+    { expiresIn: "7d" },
+  );
+
+  res.json({ message: "User logged in successfully", userObject, token });
 });
